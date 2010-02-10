07ee58bf164fff6384835be2c3379b5611acd604
Edmond Leung <leung.edmond@gmail.com>
2010-02-10 12:43:13 +0000

    Added "lazy" tracking mode.

--- js/trackme.js
+++ js/trackme.js
@@ -3,9 +3,14 @@ var TrackMe = Class.create({
         pageIdMetaTagName: "page_id",                   // name of the meta tag to grab the page id from
         regions: "#main, #sidebar, #header, #footer",   // selector that defines which regions to include in the tracking id
         articleIdRegex: "^article_",                    // regex used to test for closest article_id
-        anchorClicked: function(trackingId) {           // this function is called when an anchor is clicked (the tracking id is passed through)
+        anchorClicked: function(trackingId, event) {    // this function is called when an anchor is clicked (the tracking id is passed through)
             pageTracker._trackPageview(trackingId);
-        }
+        },
+        
+        // array that specifies a list of regex to test 
+        // on an anchors href to see if it shouldn't be tracked
+        // - Only add regexes into the array if you want to use this feature
+        hrefRegexes: []                                 
     },
 
     initialize: function(anchors, options) {        
@@ -68,47 +73,65 @@ var TrackMe = Class.create({
     },
     
     apply: function(anchor) {
-        var pageId, regionId, articleId, hrefFileType, anchorTitle;
-        
-        // Page ID
-        var metaTags = $$('meta[name=' + this.options.pageIdMetaTagName + ']');
-        if (metaTags.length > 0) {
-            var metaTag = metaTags[0];
-            pageId = metaTag.readAttribute('content');
+        var trackingId = "";
+            
+        if (this.options.hrefRegexes.length == 0) {
+            var pageId, regionId, articleId, hrefFileType, anchorTitle;
+
+            // Page ID
+            var metaTags = $$('meta[name=' + this.options.pageIdMetaTagName + ']');
+            if (metaTags.length > 0) {
+                var metaTag = metaTags[0];
+                pageId = metaTag.readAttribute('content');
+            }
+
+            // Region ID
+            var closestRegion = this.findClosestBySelector(anchor, this.options.regions);
+            if (closestRegion != null) regionId = closestRegion.readAttribute('id');
+
+            // Article ID
+            var closestArticle = this.findClosestByRegex(anchor, new RegExp(this.options.articleIdRegex));
+            if (closestArticle != null) articleId = closestArticle.readAttribute('id');
+
+            // Href File Type
+            TrackMe.PageViewTypes.each(function(pageViewType) {
+                var regex = new RegExp(pageViewType.regex);
+                if (regex.test(anchor.readAttribute('href'))) {
+                    hrefFileType = pageViewType.name;
+                }
+            });
+
+            // Anchor title
+            var title = anchor.readAttribute('title');
+            if (title !== null) {
+                anchorTitle = this.encodeURL(anchor.readAttribute('title'));   
+            }
+
+            // Final tracking ID
+            [ pageId, regionId, articleId, hrefFileType, anchorTitle ].each(function(d) {
+                if (d != null && d != "") {
+                    trackingId += "/" + d;
+                }
+            });   
+        } else {
+            // Last resort - Use regex to track external href links
+            this.options.hrefRegexes.each(function(regex) {
+                var regex = new RegExp(regex);
+                var href = anchor.readAttribute('href');
+                
+                if (!regex.test(href)) {
+                    trackingId = this.encodeURL(title || anchor.innerHTML.unescapeHTML());
+                    if (trackingId == "") { trackingId = href || ""; }
+                }
+            }.bind(this));
         }
         
-        // Region ID
-        var closestRegion = this.findClosestBySelector(anchor, this.options.regions);
-        if (closestRegion != null) regionId = closestRegion.readAttribute('id');
-        
-        // Article ID
-        var closestArticle = this.findClosestByRegex(anchor, new RegExp(this.options.articleIdRegex));
-        if (closestArticle != null) articleId = closestArticle.readAttribute('id');
-        
-        // Href File Type
-        TrackMe.PageViewTypes.each(function(pageViewType) {
-            var regex = new RegExp(pageViewType.regex);
-            if (regex.test(anchor.readAttribute('href'))) {
-                hrefFileType = pageViewType.name;
-            }
-        });
-        
-        // Anchor title
-        anchorTitle = this.encodeURL(anchor.readAttribute('title'));
-
-        // Final tracking ID
-        var trackingId = "";
-        [ pageId, regionId, articleId, hrefFileType, anchorTitle ].each(function(d) {
-            if (d != null && d != "") {
-                trackingId += "/" + d;
-            }
-        });
-        
         // Call the callback function
-        anchor.observe('click', function(event) {
-            this.options.anchorClicked.call(anchor, trackingId);
-            event.stop();
-        }.bind(this));
+        if (trackingId != "") {
+            anchor.observe('click', function(event) {
+                this.options.anchorClicked.call(anchor, trackingId, event);
+            }.bind(this));
+        }
     }
 });
 
